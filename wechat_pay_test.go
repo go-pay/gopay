3f41780754b59f79218bf886e2e5491a1520f00c
Jerry <85411418@qq.com>
2019-01-14 01:18:00 +0800

    添加沙箱测试

--- wechat_pay_test.go
+++ wechat_pay_test.go
@@ -10,21 +10,21 @@ func TestWXPay(t *testing.T) {
 	//初始化微信客户端
 	//    appId：应用ID
 	//    mchID：商户ID
+	//    secretKey：Key值
 	//    isProd：是否是正式环境
-	//    secretKey：key，（当isProd为true时，此参数必传；false时，此参数为空）
-	client := NewWeChatClient(appID, mchID, true, secretKey)
+	client := NewWeChatClient(appID, mchID, secretKey, true)
 
 	//初始化参数结构体
 	params := new(WeChatPayParams)
 	params.NonceStr = "dyUNIkNS29hvDUC1CmoF0alSdfCQGg9I"
 	params.Body = "测试充值"
-	params.OutTradeNo = "GYsadfjk4dhg3fkhffgnlsdkf"
+	params.OutTradeNo = "GYsadfjk4dhg3fk13ffgnlsdkf"
 	params.TotalFee = 10 //单位为分
 	params.SpbillCreateIp = "127.0.0.1"
 	params.NotifyUrl = "http://www.igoogle.ink"
 	params.TradeType = WX_PayType_JsApi //目前只支持JSAPI有效
 	params.DeviceInfo = "WEB"
-	params.SignType = WX_SignType_MD5 //如不设置此参数，默认为 MD5
+	params.SignType = WX_SignType_HMAC_SHA256 //如不设置此参数，默认为 MD5
 	params.Openid = openID
 
 	//请求支付下单，成功后得到结果
@@ -40,6 +40,8 @@ func TestWXPay(t *testing.T) {
 		fmt.Println("NonceStr：", wxRsp.NonceStr)
 		fmt.Println("Sign：", wxRsp.Sign)
 		fmt.Println("ResultCode：", wxRsp.ResultCode)
+		fmt.Println("ErrCode：", wxRsp.ErrCode)
+		fmt.Println("ErrCodeDes：", wxRsp.ErrCodeDes)
 		fmt.Println("PrepayId：", wxRsp.PrepayId)
 		fmt.Println("TradeType：", wxRsp.TradeType)
 	}
