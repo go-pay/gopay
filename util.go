67723772e6ee2d6d128c419f3060554643b3029d
Jerry <85411418@qq.com>
2019-01-13 15:15:47 +0800

    add function

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
-package go_pay
+package gopay
 
 import (
+	"bytes"
 	"math/rand"
 	"time"
 )
@@ -9,11 +10,29 @@ import (
 //    length：字符串长度
 func GetRandomString(length int) string {
 	str := "0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-	bytes := []byte(str)
+	b := []byte(str)
 	var result []byte
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < length; i++ {
-		result = append(result, bytes[r.Intn(len(bytes))])
+		result = append(result, b[r.Intn(len(b))])
 	}
 	return string(result)
+}
+
+func generateXml(w requestBody) (reqXml string) {
+	buffer := new(bytes.Buffer)
+	buffer.WriteString("<xml>")
+
+	for k, v := range w {
+		buffer.WriteString("<")
+		buffer.WriteString(k)
+		buffer.WriteString("><![CDATA[")
+		buffer.WriteString(v)
+		buffer.WriteString("]]></")
+		buffer.WriteString(k)
+		buffer.WriteString(">")
+	}
+	buffer.WriteString("</xml>")
+	reqXml = buffer.String()
+	return
 }
