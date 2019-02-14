bb4054fba075f903568e43cd4e73ff06361247e4
Jerry <jerry@iguiyu.com>
2019-02-14 17:57:47 +0800

    add new url

--- constant.go
+++ constant.go
@@ -10,10 +10,12 @@ const (
 	wx_sanbox_base_url = "https://api.mch.weixin.qq.com/sandboxnew/"
 
 	//正式
+	wxURL_Micropay          = wx_base_url + "pay/micropay"                    //提交付款码支付
 	wxURL_UnifiedOrder      = wx_base_url + "pay/unifiedorder"                //统一下单
 	wxURL_OrderQuery        = wx_base_url + "pay/orderquery"                  //查询订单
 	wxURL_CloseOrder        = wx_base_url + "pay/closeorder"                  //关闭订单
 	wxURL_Refund            = wx_base_url + "secapi/pay/refund"               //申请退款
+	wxURL_Reverse           = wx_base_url + "secapi/pay/reverse"              //撤销订单
 	wxURL_RefundQuery       = wx_base_url + "pay/refundquery"                 //查询退款
 	wxURL_DownloadBill      = wx_base_url + "pay/downloadbill"                //下载对账单
 	wxURL_DownloadFundFlow  = wx_base_url + "pay/downloadfundflow"            //下载资金账单
@@ -21,10 +23,12 @@ const (
 
 	//SanBox
 	wxURL_SanBox_GetSignKey        = wx_sanbox_base_url + "pay/getsignkey"
+	wxURL_SanBox_Micropay          = wx_sanbox_base_url + "pay/micropay"
 	wxURL_SanBox_UnifiedOrder      = wx_sanbox_base_url + "pay/unifiedorder"
 	wxURL_SanBox_OrderQuery        = wx_sanbox_base_url + "pay/orderquery"
 	wxURL_SanBox_CloseOrder        = wx_sanbox_base_url + "pay/closeorder"
 	wxURL_SanBox_Refund            = wx_sanbox_base_url + "pay/refund"
+	wxURL_SanBox_Reverse           = wx_sanbox_base_url + "pay/reverse"
 	wxURL_SanBox_RefundQuery       = wx_sanbox_base_url + "pay/refundquery"
 	wxURL_SanBox_DownloadBill      = wx_sanbox_base_url + "pay/downloadbill"
 	wxURL_SanBox_DownloadFundFlow  = wx_sanbox_base_url + "pay/downloadfundflow"
