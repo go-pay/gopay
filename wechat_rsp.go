14b3bebe380043e8f0977c7aeacee15e79d21c60
Jerry <jerry@iguiyu.com>
2019-05-25 20:35:34 +0800

    add struct

--- wechat_rsp.go
+++ wechat_rsp.go
@@ -222,15 +222,49 @@ type Code2SessionRsp struct {
 	Errmsg     string `json:"errmsg"`      //错误信息
 }
 
-type GetPaidUnionIdRsp struct {
+type PaidUnionId struct {
 	Unionid string `json:"unionid"` //用户在开放平台的唯一标识符
 	Errcode int    `json:"errcode"` //错误码
 	Errmsg  string `json:"errmsg"`  //错误信息
 }
 
-type GetAccessTokenRsp struct {
+type AccessToken struct {
 	AccessToken string `json:"access_token"` //获取到的凭证
 	ExpiresIn   int    `json:"expires_in"`   //SessionKey超时时间（秒）
 	Errcode     int    `json:"errcode"`      //错误码
 	Errmsg      string `json:"errmsg"`       //错误信息
+}
+
+type WeChatUserInfo struct {
+	Subscribe      int    `json:"subscribe"`       //用户是否订阅该公众号标识，值为0时，代表此用户没有关注该公众号，拉取不到其余信息。
+	Openid         string `json:"openid"`          //用户唯一标识
+	Nickname       string `json:"nickname"`        //用户的昵称
+	Sex            int    `json:"sex"`             //用户的性别，值为1时是男性，值为2时是女性，值为0时是未知
+	Language       string `json:"language"`        //用户的语言，简体中文为zh_CN
+	City           string `json:"city"`            //用户所在城市
+	Province       string `json:"province"`        //用户所在省份
+	Country        string `json:"country"`         //	用户所在国家
+	Headimgurl     string `json:"headimgurl"`      //用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空。若用户更换头像，原有头像URL将失效。
+	SubscribeTime  int    `json:"subscribe_time"`  //用户关注时间，为时间戳。如果用户曾多次关注，则取最后关注时间
+	Unionid        string `json:"unionid"`         //只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段。
+	Remark         string `json:"remark"`          //公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注
+	Groupid        int    `json:"groupid"`         //用户所在的分组ID（兼容旧的用户分组接口）
+	TagidList      []int  `json:"tagid_list"`      //用户被打上的标签ID列表
+	SubscribeScene string `json:"subscribe_scene"` //返回用户关注的渠道来源，ADD_SCENE_SEARCH 公众号搜索，ADD_SCENE_ACCOUNT_MIGRATION 公众号迁移，ADD_SCENE_PROFILE_CARD 名片分享，ADD_SCENE_QR_CODE 扫描二维码，ADD_SCENEPROFILE LINK 图文页内名称点击，ADD_SCENE_PROFILE_ITEM 图文页右上角菜单，ADD_SCENE_PAID 支付后关注，ADD_SCENE_OTHERS 其他
+	QrScene        int    `json:"qr_scene"`        //二维码扫码场景（开发者自定义）
+	QrSceneStr     string `json:"qr_scene_str"`    //二维码扫码场景描述（开发者自定义）
+	Errcode        int    `json:"errcode"`         //错误码
+	Errmsg         string `json:"errmsg"`          //错误信息
+}
+
+type WeChatUserPhone struct {
+	PhoneNumber     string        `json:"phoneNumber"`
+	PurePhoneNumber string        `json:"purePhoneNumber"`
+	CountryCode     string        `json:"countryCode"`
+	Watermark       watermarkInfo `json:"watermark"`
+}
+
+type watermarkInfo struct {
+	Appid     string `json:"appid"`
+	Timestamp int    `json:"timestamp"`
 }
