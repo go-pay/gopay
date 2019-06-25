917aaed57734685b69d5293c175a1516e4b1c6b3
Jerry <jerry@iguiyu.com>
2019-06-25 23:49:59 +0800

    fix

--- alipay_client_test.go
+++ alipay_client_test.go
@@ -23,7 +23,7 @@ func TestAliPayClient_AliPayTradeWapPay(t *testing.T) {
 	//请求参数
 	body := make(BodyMap)
 	body.Set("subject", "手机网站测试支付")
-	body.Set("out_trade_no", "GYWX201901301040355706100440")
+	body.Set("out_trade_no", "GYWX201901301040355706100443")
 	body.Set("quit_url", "https://www.igoogle.ink")
 	body.Set("total_amount", "100.00")
 	body.Set("product_code", "QUICK_WAP_WAY")
