33c55248f477123d343af8de08a8dcfb413590de
Jerry <fumingming@bilibili.com>
2019-12-11 16:07:57 +0800

    no message

--- examples/wechat/wx_UnifiedOrder.go
+++ examples/wechat/wx_UnifiedOrder.go
@@ -44,8 +44,10 @@ func UnifiedOrder() {
 
 	//body.Set("openid", "o0Df70H2Q0fY8JXh1aFPIRyOBgu8")
 
-	//sign := gopay.GetParamSign("wxdaa2ab9ef87b5497", "1368139502", "GFDS8j98rewnmgl45wHTt980jg543abc", body)
-	//sign, _ := gopay.GetSanBoxParamSign("wxdaa2ab9ef87b5497", "1368139502", "GFDS8j98rewnmgl45wHTt980jg543abc", body)
+	// 正式
+	//sign := gopay.GetWeChatParamSign("wxdaa2ab9ef87b5497", "1368139502", "GFDS8j98rewnmgl45wHTt980jg543abc", body)
+	// 沙箱
+	//sign, _ := gopay.GetWeChatSanBoxParamSign("wxdaa2ab9ef87b5497", "1368139502", "GFDS8j98rewnmgl45wHTt980jg543abc", body)
 	//body.Set("sign", sign)
 
 	//请求支付下单，成功后得到结果
