d714d5680a6e8f11324c0d7ea646e5dd98106026
fangxiaokai <fangxiaokai2006@qq.com>
2020-07-08 18:06:08 +0800

    分账功能，重新提交

--- wechat/client.go
+++ wechat/client.go
@@ -537,7 +537,7 @@ func (w *Client) MultiProfitSharing(bm gopay.BodyMap) (wxRsp *ProfitSharingRespo
 }
 
 func (w *Client) profitSharing(bm gopay.BodyMap, uri string) (wxRsp *ProfitSharingResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "transaction_id", "out_order_no", "receivers")
+	err = bm.CheckEmptyError("nonce_str", "transaction_id", "out_order_no", "receivers")
 	if err != nil {
 		return nil, err
 	}
@@ -571,7 +571,7 @@ func (w *Client) profitSharing(bm gopay.BodyMap, uri string) (wxRsp *ProfitShari
 // 微信文档：https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_2&index=3
 // 发起分账请求后，可调用此接口查询分账结果；发起分账完结请求后，可调用此接口查询分账完结的执行结果。
 func (w *Client) ProfitSharingQuery(bm gopay.BodyMap) (wxRsp *ProfitSharingQueryResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "transaction_id", "out_order_no", "nonce_str")
+	err = bm.CheckEmptyError("transaction_id", "out_order_no", "nonce_str")
 	if err != nil {
 		return nil, err
 	}
@@ -590,7 +590,7 @@ func (w *Client) ProfitSharingQuery(bm gopay.BodyMap) (wxRsp *ProfitSharingQuery
 // 微信文档：https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_3&index=4
 // 商户发起添加分账接收方请求，后续可通过发起分账请求将结算后的钱分到该分账接收方。
 func (w *Client) ProfitSharingAddReceiver(bm gopay.BodyMap) (wxRsp *ProfitSharingAddReceiverResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "receiver")
+	err = bm.CheckEmptyError("nonce_str", "receiver")
 	if err != nil {
 		return nil, err
 	}
@@ -618,7 +618,7 @@ func (w *Client) ProfitSharingAddReceiver(bm gopay.BodyMap) (wxRsp *ProfitSharin
 // 微信文档：https://pay.weixin.qq.com/wiki/doc/api/allocation.php?chapter=27_4&index=5
 // 商户发起删除分账接收方请求，删除后不支持将结算后的钱分到该分账接收方
 func (w *Client) ProfitSharingRemoveReceiver(bm gopay.BodyMap) (wxRsp *ProfitSharingAddReceiverResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "receiver")
+	err = bm.CheckEmptyError("nonce_str", "receiver")
 	if err != nil {
 		return nil, err
 	}
@@ -648,7 +648,7 @@ func (w *Client) ProfitSharingRemoveReceiver(bm gopay.BodyMap) (wxRsp *ProfitSha
 // 2、调用多次分账接口后，需要解冻剩余资金时，调用本接口将剩余的分账金额全部解冻给特约商户
 // 3、已调用请求单次分账后，剩余待分账金额为零，不需要再调用此接口。
 func (w *Client) ProfitSharingFinish(bm gopay.BodyMap) (wxRsp *ProfitSharingResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "transaction_id", "out_order_no", "description")
+	err = bm.CheckEmptyError("nonce_str", "transaction_id", "out_order_no", "description")
 	if err != nil {
 		return nil, err
 	}
@@ -671,7 +671,7 @@ func (w *Client) ProfitSharingFinish(bm gopay.BodyMap) (wxRsp *ProfitSharingResp
 // 此接口采用同步处理模式，即在接收到商户请求后，会实时返回处理结果
 // 此功能需要接收方在商户平台-交易中心-分账-分账接收设置下，开启同意分账回退后，才能使用。
 func (w *Client) ProfitSharingReturn(bm gopay.BodyMap) (wxRsp *ProfitSharingReturnResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "out_return_no", "return_account_type", "return_account", "return_amount", "description")
+	err = bm.CheckEmptyError("nonce_str", "out_return_no", "return_account_type", "return_account", "return_amount", "description")
 	if err != nil {
 		return nil, err
 	}
@@ -696,7 +696,7 @@ func (w *Client) ProfitSharingReturn(bm gopay.BodyMap) (wxRsp *ProfitSharingRetu
 // 商户需要核实回退结果，可调用此接口查询回退结果。
 // 如果分账回退接口返回状态为处理中，可调用此接口查询回退结果
 func (w *Client) ProfitSharingReturnQuery(bm gopay.BodyMap) (wxRsp *ProfitSharingReturnResponse, err error) {
-	err = bm.CheckEmptyError("mch_id", "appid", "nonce_str", "out_return_no")
+	err = bm.CheckEmptyError("nonce_str", "out_return_no")
 	if err != nil {
 		return nil, err
 	}
