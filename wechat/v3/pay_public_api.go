41aadee429a050bba77bffbe62addfb6623ef16e
cr <cr>
2021-02-04 16:01:15 +0800

    refund

--- wechat/v3/pay_public_api.go
+++ wechat/v3/pay_public_api.go
@@ -580,3 +580,39 @@ func (c *ClientV3) V3CombineTransactionCloseOrder(tradeNo string, bm gopay.BodyM
 	}
 	return wxRsp, c.verifySyncSign(si)
 }
+
+// 申请退款API
+//	Code = 0 is success
+//	文档：https://pay.weixin.qq.com/wiki/doc/apiv3/apis/chapter5_1_14.shtml
+func (c *ClientV3) V3Refund(bm gopay.BodyMap) (wxRsp *RefundRsp, err error) {
+	var (
+		ts       = time.Now().Unix()
+		nonceStr = util.GetRandomString(32)
+		url      = v3Refund
+	)
+	if bm != nil {
+		if bm.Get("combine_appid") == util.NULL {
+			bm.Set("combine_appid", c.Appid)
+		}
+	}
+	authorization, err := c.authorization(MethodPost, url, nonceStr, ts, bm)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdPost(bm, url, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp = &RefundRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(RefundOrderResponse)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
