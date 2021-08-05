a8b0ef289b63938fd1db38530aa426c45bfdcdcf
Jerry <85411418@qq.com>
2021-08-05 20:37:37 +0800

    fix

--- wechat/v3/client_test.go
+++ wechat/v3/client_test.go
@@ -323,7 +323,9 @@ func TestV3ProfitSharingDeleteReceiver(t *testing.T) {
 }
 
 func TestV3ProfitSharingQuery(t *testing.T) {
-	wxRsp, err := client.V3ProfitShareOrderQuery("202106071738581340", "4200001149202106084654939138")
+	bm := make(gopay.BodyMap)
+	bm.Set("transaction_id", 4200001149202106084654939138)
+	wxRsp, err := client.V3ProfitShareOrderQuery("P20150806125346", bm)
 	if err != nil {
 		xlog.Error(err)
 		return
