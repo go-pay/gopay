920439d12476ec833d80376cdcdfb22a352f77dc
Jerry <85411418@qq.com>
2021-08-22 00:46:42 +0800

    fix

--- gopay_test.go
+++ gopay_test.go
@@ -1,9 +1,6 @@
 package gopay
 
 import (
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"encoding/xml"
 	"testing"
 	"time"
@@ -107,75 +104,4 @@ func TestBodyMap_UnmarshalXML2(t *testing.T) {
 	for _, v := range mm.Hblist.HbinfoList {
 		xlog.Debugf("%+v", v)
 	}
-}
-
-func TestPkcs(t *testing.T) {
-	var PriKey = `-----BEGIN PRIVATE KEY-----
-MIIEwAIBADANBgkqhkiG9w0BAQEFAASCBKowggSmAgEAAoIBAQDV523KVXZaaZI3
-WxQiaz0J8o8nxAYsxBjrfcaKPnLo+r5uFME7GPV+4UHEZWG6ZogJ87yBt8L4IV8q
-/2n0MPKV5qNtS0htG7G0Mtyw7lPmdXUXsA0ionbL2mzz0kgJ1S6FJwyZRRZNJ08Q
-/GQE3TWqErbxL/2ITuzTeHrdTNL0i9oNxtB92EWFZ0gSL677zEiz41EVog24SyOd
-TFqxjGFd9DR0CeRNU/oQPplFnM9YFseRuhEZ/jLATEvubH/U1qGqTlW0UHvIn14j
-NqRxyAjDI/HfXl3Bo7Fx0QCJkVkqb+5ou8KFRchbcixRU0khbrxTy7dDJj60vSmr
-PySqqZLFAgMBAAECggEBAKHPN9ZfX/B0/A6z7z86MCpeOryyJJmondFGi/H326Uy
-SOus959k+hDJBZ8zsgH3neEpZ+gYwnxBgmRcYiI/BMMwfWAoGtmuoXbXIusU3pLv
-N2x72PPiQktjKBgpciU+BrrjFzy6bmxe2AjZZC/pxrapAYrh6sA6NBykfwz5GHu0
-DQmjHYqSlghDDljCzVR3Gcs/KicCMw6eQ0JlWDqtDEDoENlBkfn4spHwocV7HtSq
-0bnUrQqqMtpZjbMJzZxJc39qkyNNDosuNy5GXYLQE7lr9RuRqLxEfg6KfGUS5bAZ
-eJ5pizql7+c0viUtiXG17PYp8QR4c5G+54RlQd1pPuECgYEA9UBi5rFJzK0/n4aO
-lsrp6BvUOSherp57SNYvpsRuBPU0odyH2/McLNphisKTxfSm0/hADaTmnzAnOUVg
-cduc/5/5tVaaqyLL3SemxJhwqVsL3tE/KAN7HUBhhQrqD+H8r39TAoIkyfjCOHzS
-74rygZ35x0kXNMavXQFB0RE2fEcCgYEA30dWaLddGmTvUXwhyTWcsiDfrsKbw8+n
-MhAlSCXE42v9Uo3ULqD3/rpUQlMhoqaZb3cSyOyQwJvv0tp/g3hM7Q4usLxkdysc
-KA9HmmZ4Q2P2838cqvNr/Dz1UAnfdDryMEnbiv1MUKYqFFTVX6oR0iH544JgDFCG
-YLQA2M+3GpMCgYEAh+ax51v+pSirxN5vTSgMDc69/x5buS+g6W+m4CahQKYQEFGA
-B2XkCwbIXngMIvm7KGK8O9NQ6I1qbtX+55jmmtAvM0lWU9boWRiL1Q0UAQSuwz34
-XVfwdPkkEPFHWp3DxAwuF4m+kR0DowGocYzxbNn5e3EJJvmiW0tDCXMcWikCgYEA
-tyNxWcUFBdBCh+i0YbCqzWSvdE3Fq8/YSPT7T3lDTHLYPu18W57Gq1Y0JI7BaQMT
-mVzmuI1pkcKV7LIxoyl6l3ppi6eLFD/1AVq/FYL1I/mLpl/dqM6vBR8O686dTV3I
-Jxl9jTyEayZQH4sR1TzPDze1GwpmM9Oc1RbwFuYRPycCgYEAzYaRKh6EQ+s37HDv
-e/ZGMs3PI+CoA/x6lx4Owa7amRsWRKys45NV6gcC8pkbN4IeFaYXVHmJ1Yaef3xn
-0VxHAzWI4BF+1pUwXzS2rAMBZR/VKS0XA856NauAC3mKHipoOWVVs+uFP3VMUQ79
-hSImAa7UBzss6b6ie7AYxXtZBjY=
------END PRIVATE KEY-----`
-
-	var (
-		pk *rsa.PrivateKey
-		ok bool
-	)
-	block, _ := pem.Decode([]byte(PriKey))
-	if block == nil {
-		return
-	}
-	xlog.Debugf("%+v", block.Type)
-	//pk8, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	//if err != nil {
-	//	xlog.Errorf("1.err:%+v",err)
-	//	pk, err = x509.ParsePKCS1PrivateKey(block.Bytes)
-	//	if err != nil {
-	//		xlog.Errorf("2.err:%+v",err)
-	//		return
-	//	}
-	//}
-	pk, err := x509.ParsePKCS1PrivateKey(block.Bytes)
-	if err != nil {
-		xlog.Errorf("1.err:%+v", err)
-		pk8, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-		if err != nil {
-			xlog.Errorf("2.err:%+v", err)
-			return
-		}
-		pk, ok = pk8.(*rsa.PrivateKey)
-		if !ok {
-			xlog.Warn("3.err:")
-			return
-		}
-	}
-	xlog.Debugf("%+v", pk.PublicKey)
-	//xlog.Debugf("%+v", pub)
-	//publicKey, ok := pub.PublicKey.(*rsa.PublicKey)
-	//if !ok {
-	//	return
-	//}
-	//key := x509.MarshalPKCS1PublicKey(publicKey)
 }
