2a4290bbf4a424e309bb0bc1a6e1651073a7262c
Jerry <85411418@qq.com>
2021-08-27 13:04:17 +0800

    fix

--- wechat/v3/sign_test.go
+++ wechat/v3/sign_test.go
@@ -2,18 +2,12 @@ package wechat
 
 import (
 	"testing"
-	"time"
-
-	"github.com/go-pay/gopay/pkg/util"
 
 	"github.com/go-pay/gopay/pkg/xlog"
 )
 
 func TestPaySignOfJSAPIp(t *testing.T) {
-	jsapi, err := client.PaySignOfJSAPI("appid", "prepayid", &SignInfo{
-		HeaderTimestamp: util.Int642String(time.Now().Unix()),
-		HeaderNonce:     util.GetRandomString(32),
-	})
+	jsapi, err := client.PaySignOfJSAPI("appid", "prepayid")
 	if err != nil {
 		xlog.Error(err)
 		return
