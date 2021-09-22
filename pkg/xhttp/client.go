cf03d5726a2ca54ffbc6608ea7f65df91dbeb737
Jerry <85411418@qq.com>
2021-09-22 15:51:25 +0800

    Update FormatURLParam Function

--- pkg/xhttp/client.go
+++ pkg/xhttp/client.go
@@ -12,6 +12,7 @@ import (
 	"mime/multipart"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 	"time"
 
@@ -418,9 +419,43 @@ func (c *Client) EndBytes() (res *http.Response, bs []byte, errs []error) {
 }
 
 func FormatURLParam(body map[string]interface{}) (urlParam string) {
-	v := url.Values{}
-	for key, value := range body {
-		v.Add(key, value.(string))
+	var (
+		buf  strings.Builder
+		keys []string
+	)
+	for k := range body {
+		keys = append(keys, k)
 	}
-	return v.Encode()
+	sort.Strings(keys)
+	for _, k := range keys {
+		v, ok := body[k].(string)
+		if !ok {
+			v = convertToString(body[k])
+		}
+		if v != "" {
+			buf.WriteString(url.QueryEscape(k))
+			buf.WriteByte('=')
+			buf.WriteString(url.QueryEscape(v))
+			buf.WriteByte('&')
+		}
+	}
+	if buf.Len() <= 0 {
+		return ""
+	}
+	return buf.String()[:buf.Len()-1]
+}
+
+func convertToString(v interface{}) (str string) {
+	if v == nil {
+		return ""
+	}
+	var (
+		bs  []byte
+		err error
+	)
+	if bs, err = json.Marshal(v); err != nil {
+		return ""
+	}
+	str = string(bs)
+	return
 }
