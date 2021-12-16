95f7abaf054c2ede275e61f7c91a16e9b0fb4126
hujiali <375748201@qq.com>
2021-12-16 23:16:29 +0800

    feat: partner_transfer

--- wechat/v3/transfer.go
+++ wechat/v3/transfer.go
@@ -13,7 +13,6 @@ import (
 //	注意：入参加密字段数据加密：client.V3EncryptText()
 //	Code = 0 is success
 // 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_1.shtml
-//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_1.shtml
 func (c *ClientV3) V3Transfer(ctx context.Context, bm gopay.BodyMap) (*TransferRsp, error) {
 	authorization, err := c.authorization(MethodPost, v3Transfer, bm)
 	if err != nil {
@@ -37,10 +36,36 @@ func (c *ClientV3) V3Transfer(ctx context.Context, bm gopay.BodyMap) (*TransferR
 	return wxRsp, c.verifySyncSign(si)
 }
 
+// 发起批量转账API（服务商）
+//	注意：入参加密字段数据加密：client.V3EncryptText()
+//	Code = 0 is success
+//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_1.shtml
+func (c *ClientV3) V3PartnerTransfer(ctx context.Context, bm gopay.BodyMap) (*TransferRsp, error) {
+	authorization, err := c.authorization(MethodPost, v3PartnerTransfer, bm)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdPost(ctx, bm, v3PartnerTransfer, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp := &TransferRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(Transfer)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
+
 // 微信批次单号查询批次单API
 //	Code = 0 is success
 // 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_2.shtml
-//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_2.shtml
 func (c *ClientV3) V3TransferQuery(ctx context.Context, batchId string, bm gopay.BodyMap) (*TransferQueryRsp, error) {
 	url := fmt.Sprintf(v3TransferQuery, batchId)
 	bm.Remove("batch_id")
@@ -67,12 +92,40 @@ func (c *ClientV3) V3TransferQuery(ctx context.Context, batchId string, bm gopay
 	return wxRsp, c.verifySyncSign(si)
 }
 
+// 微信批次单号查询批次单API（服务商）
+//	Code = 0 is success
+//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_2.shtml
+func (c *ClientV3) V3PartnerTransferQuery(ctx context.Context, batchId string, bm gopay.BodyMap) (*PartnerTransferQueryRsp, error) {
+	url := fmt.Sprintf(v3PartnerTransferQuery, batchId)
+	bm.Remove("batch_id")
+	uri := url + "?" + bm.EncodeURLParams()
+	authorization, err := c.authorization(MethodGet, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdGet(ctx, uri, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp := &PartnerTransferQueryRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(PartnerTransferQuery)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
+
 // 微信明细单号查询明细单API
 //	Code = 0 is success
 // 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_3.shtml
-//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_3.shtml
-func (c *ClientV3) V3TransferDetailQuery(ctx context.Context, batchId, detailId string) (*TransferDetailQueryRsp, error) {
-	url := fmt.Sprintf(v3TransferDetailQuery, batchId, detailId)
+func (c *ClientV3) V3TransferDetail(ctx context.Context, batchId, detailId string) (*TransferDetailRsp, error) {
+	url := fmt.Sprintf(v3TransferDetail, batchId, detailId)
 	authorization, err := c.authorization(MethodGet, url, nil)
 	if err != nil {
 		return nil, err
@@ -82,7 +135,7 @@ func (c *ClientV3) V3TransferDetailQuery(ctx context.Context, batchId, detailId 
 		return nil, err
 	}
 
-	wxRsp := &TransferDetailQueryRsp{Code: Success, SignInfo: si}
+	wxRsp := &TransferDetailRsp{Code: Success, SignInfo: si}
 	wxRsp.Response = new(TransferDetailQuery)
 	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
 		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
@@ -95,10 +148,42 @@ func (c *ClientV3) V3TransferDetailQuery(ctx context.Context, batchId, detailId 
 	return wxRsp, c.verifySyncSign(si)
 }
 
+// 微信明细单号查询明细单API（服务商）
+//	Code = 0 is success
+//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_3.shtml
+func (c *ClientV3) V3PartnerTransferDetail(ctx context.Context, batchId, detailId string) (*PartnerTransferDetailRsp, error) {
+	url := fmt.Sprintf(v3PartnerTransferDetail, batchId, detailId)
+	authorization, err := c.authorization(MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdGet(ctx, url, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp := &PartnerTransferDetailRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(PartnerTransferDetail)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
+
+// Deprecated
+// 推荐直接使用 client.V3TransferDetail() 方法
+func (c *ClientV3) V3TransferDetailQuery(ctx context.Context, batchId, detailId string) (*TransferDetailRsp, error) {
+	return c.V3TransferDetail(ctx, batchId, detailId)
+}
+
 // 商家批次单号查询批次单API
 //	Code = 0 is success
 // 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_4.shtml
-//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_4.shtml
 func (c *ClientV3) V3TransferMerchantQuery(ctx context.Context, outBatchNo string, bm gopay.BodyMap) (*TransferMerchantQueryRsp, error) {
 	url := fmt.Sprintf(v3TransferMerchantQuery, outBatchNo)
 	bm.Remove("out_batch_no")
@@ -125,23 +210,24 @@ func (c *ClientV3) V3TransferMerchantQuery(ctx context.Context, outBatchNo strin
 	return wxRsp, c.verifySyncSign(si)
 }
 
-// 商家明细单号查询明细单API
+// 商家批次单号查询批次单API（服务商）
 //	Code = 0 is success
-// 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_5.shtml
-//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_5.shtml
-func (c *ClientV3) V3TransferMerchantDetailQuery(ctx context.Context, outBatchNo, outDetailNo string) (*TransferMerchantDetailQueryRsp, error) {
-	url := fmt.Sprintf(v3TransferMerchantDetailQuery, outBatchNo, outDetailNo)
-	authorization, err := c.authorization(MethodGet, url, nil)
+//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_4.shtml
+func (c *ClientV3) V3PartnerTransferMerchantQuery(ctx context.Context, outBatchNo string, bm gopay.BodyMap) (*PartnerTransferMerchantQueryRsp, error) {
+	url := fmt.Sprintf(v3PartnerTransferMerchantQuery, outBatchNo)
+	bm.Remove("out_batch_no")
+	uri := url + "?" + bm.EncodeURLParams()
+	authorization, err := c.authorization(MethodGet, uri, nil)
 	if err != nil {
 		return nil, err
 	}
-	res, si, bs, err := c.doProdGet(ctx, url, authorization)
+	res, si, bs, err := c.doProdGet(ctx, uri, authorization)
 	if err != nil {
 		return nil, err
 	}
 
-	wxRsp := &TransferMerchantDetailQueryRsp{Code: Success, SignInfo: si}
-	wxRsp.Response = new(TransferMerchantDetailQuery)
+	wxRsp := &PartnerTransferMerchantQueryRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(PartnerTransferMerchantQuery)
 	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
 		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
 	}
@@ -151,6 +237,66 @@ func (c *ClientV3) V3TransferMerchantDetailQuery(ctx context.Context, outBatchNo
 		return wxRsp, nil
 	}
 	return wxRsp, c.verifySyncSign(si)
+}
+
+// 商家明细单号查询明细单API
+//	Code = 0 is success
+// 	商户文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer/chapter3_5.shtml
+func (c *ClientV3) V3TransferMerchantDetail(ctx context.Context, outBatchNo, outDetailNo string) (*TransferMerchantDetailRsp, error) {
+	url := fmt.Sprintf(v3TransferMerchantDetail, outBatchNo, outDetailNo)
+	authorization, err := c.authorization(MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdGet(ctx, url, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp := &TransferMerchantDetailRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(TransferMerchantDetail)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
+
+// 商家明细单号查询明细单API（服务商）
+//	Code = 0 is success
+//	服务商文档：https://pay.weixin.qq.com/wiki/doc/apiv3/wxpay/pay/transfer_partner/chapter3_5.shtml
+func (c *ClientV3) V3PartnerTransferMerchantDetail(ctx context.Context, outBatchNo, outDetailNo string) (*PartnerTransferMerchantDetailRsp, error) {
+	url := fmt.Sprintf(v3PartnerTransferMerchantDetail, outBatchNo, outDetailNo)
+	authorization, err := c.authorization(MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, si, bs, err := c.doProdGet(ctx, url, authorization)
+	if err != nil {
+		return nil, err
+	}
+
+	wxRsp := &PartnerTransferMerchantDetailRsp{Code: Success, SignInfo: si}
+	wxRsp.Response = new(PartnerTransferMerchantDetail)
+	if err = json.Unmarshal(bs, wxRsp.Response); err != nil {
+		return nil, fmt.Errorf("json.Unmarshal(%s)：%w", string(bs), err)
+	}
+	if res.StatusCode != http.StatusOK {
+		wxRsp.Code = res.StatusCode
+		wxRsp.Error = string(bs)
+		return wxRsp, nil
+	}
+	return wxRsp, c.verifySyncSign(si)
+}
+
+// Deprecated
+// 推荐直接使用 client.V3TransferMerchantDetail() 方法
+func (c *ClientV3) V3TransferMerchantDetailQuery(ctx context.Context, outBatchNo, outDetailNo string) (*TransferMerchantDetailRsp, error) {
+	return c.V3TransferMerchantDetail(ctx, outBatchNo, outDetailNo)
 }
 
 // 转账电子回单申请受理API
