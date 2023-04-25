66ebb37a496de906da95c1a2ecd7f7d9fed73fb1
frankhe <hy616727747@qq.com>
2023-04-26 00:13:52 +0800

    微信修改结算账号受理模式API (#315)
    * 微信支付修改结算账号添加受理模式API

--- wechat/v3/model_applyment.go
+++ wechat/v3/model_applyment.go
@@ -16,6 +16,22 @@ type Apply4SubQueryRsp struct {
 	Error    string          `json:"-"`
 }
 
+// 特约商户/二级商户修改结算账号 Rsp
+type Apply4SubModifySettlementRsp struct {
+	Code     int                        `json:"-"`
+	SignInfo *SignInfo                  `json:"-"`
+	Response *Apply4SubModifySettlement `json:"response,omitempty"`
+	Error    string                     `json:"-"`
+}
+
+// 查询结算账户修改申请状态 Rsp
+type V3Apply4SubMerchantsApplicationRsp struct {
+	Code     int                              `json:"-"`
+	SignInfo *SignInfo                        `json:"-"`
+	Response *V3Apply4SubMerchantsApplication `json:"response,omitempty"`
+	Error    string                           `json:"-"`
+}
+
 // 特约商户查询结算账号 Rsp
 type Apply4SubQuerySettlementRsp struct {
 	Code     int                       `json:"-"`
@@ -46,6 +62,10 @@ type Applyment4SubAuditDetail struct {
 	RejectReason string `json:"reject_reason"` // 驳回原因
 }
 
+type Apply4SubModifySettlement struct {
+	ApplicationNo string `json:"application_no"` //修改结算账户申请单号 提交二级商户修改结算账户申请后，由微信支付返回的单号，作为查询申请状态的唯一标识。
+}
+
 type Apply4SubQuerySettlement struct {
 	AccountType      string `json:"account_type"`       // 账户类型
 	AccountBank      string `json:"account_bank"`       // 开户银行
@@ -54,4 +74,16 @@ type Apply4SubQuerySettlement struct {
 	AccountNumber    string `json:"account_number"`     // 银行账号
 	VerifyResult     string `json:"verify_result"`      // 汇款验证结果
 	VerifyFailReason string `json:"verify_fail_reason"` // 汇款验证失败原因
+}
+
+type V3Apply4SubMerchantsApplication struct {
+	AccountName      string `json:"account_name"`       // 开户名称
+	AccountType      string `json:"account_type"`       // 账户类型
+	AccountBank      string `json:"account_bank"`       // 开户银行
+	BankName         string `json:"bank_name"`          // 开户银行全称（含支行）
+	BankBranchId     string `json:"bank_branch_id"`     // 开户银行联行号
+	AccountNumber    string `json:"account_number"`     // 银行账号
+	VerifyResult     string `json:"verify_result"`      // 汇款验证结果
+	VerifyFailReason string `json:"verify_fail_reason"` // 汇款验证失败原因
+	VerifyFinishTime string `json:"verify_finish_time"` // 审核结果更新时间
 }
