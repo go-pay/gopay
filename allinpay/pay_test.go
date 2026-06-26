7bf22dc11abe52137810885914daff5b2e72f3b6
Shutian.Ka <shutian_ka@163.com>
2026-06-26 11:35:26 +0800

    test(allinpay): 更新支付测试用例配置
    - 为 ScanPay 测试函数添加客户端初始化代码
    - 为 NativePay 测试函数添加客户端初始化代码
    - 将错误处理从 xlog 日志改为 t.Fatal 断言
    - 移除调试日志输出，改用 t.Logf 记录响应信息
    - 统一测试用例中的密钥配置信息

--- allinpay/pay_test.go
+++ allinpay/pay_test.go
@@ -10,6 +10,14 @@ import (
 )
 
 func TestClient_ScanPay(t *testing.T) {
+	client, err = NewClient("660121065134QCZ", "00382908",
+		"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1izXRKdSXuwse3SDP9xDoHg3UnvNOq8RhcU/5479w5pYJiSTCkOrC45+2SsdItwJSxxZOnvRTZl46n/q/IoO5HDWSk45Ox2jY9Adii7GPyBJxl0Bl+skTKkMUovfbc1G4Yy+2mwhRd3JD7MWs42M/d91ENFYyQV9QF3f77TKriZX20o0apthnC88L2AFnBG8CkzK+B2izoMmbkffhC2L088UZQYUHHpC/GjLVcgdHwEzIlRSHwy1rn7PmYJ6s7hiwXd9Q/uqjncCJ9yunvwvxs6y00F2T9hvU7hvjn5xlW6DSY/MkXYLVblNWhYmD0ivCeKtx96SjdBocUXLG9WZHAgMBAAECggEAAfjIMtvAizBCKnsbSKzKHsQEzPusQfGUwKUZhAEhcHOTnIRbiyC6XX1ZnAIrjWbQ6rwopyPGHKyxma1om0HCtlGhZA63UYWMN58Ho1KN1BKB31JVyOiuxgzhGOnrf57fwaAH/BeTWHZywOhLz4ejo8czJVAiwbfjsDkCfMrp1iiQ/lLI+npHYnG9qGAuUqa9C0NSJlTP+tG1m4DBkodJIXPsmTmqTudav1cQKsMQr8zcf8yWtAdB9XhqTwqLAiLR76w2KWwRSxjtjCaX8QTITcjWX2Rpj4iCmuFT0qy0THZRwURX2faXO3GTg7e23wTJ6GdPmYqCbGME+ZSAvm81wQKBgQDa3Vo8qYYRcqraSJPbdFfQUR4DXwsdgdj/39dCSxIAWlYAKdsZiWjfwh3u69ENZxKYjEvJvil78KIahqggNAobARDV+2mSdsnTn0UxtTGxbF+AGXVARDK+GRXu/aN1qpuOBoXXRmTWfKLIsRnBjT2m4u14Rs+0shT17ByGcyB5FwKBgQDUWMgDe2ZV9oYxAo5PbrqXkLJ4Vw3nBPg4ETGHFvDPmrHTTL8ww0ZT4qLb4Bi0145GVvdf4fB4Q6rUfEBKTwmhHLW4HxjIGGmAPmiTfaMLhcS370uZ9q1YddYoMIkbpsfG1Ovlaw3BpsaN6HYIG7aD69s8Rzv1kiswqIPc0etaUQKBgQCxCj8VNPgbIwtreSwJaAokm4lQptAh9UgoatQAbyNHn8tTZIg0Fv/7iFWYT68STV8wgMRJlAaQmC04kdZr/kxyXaFVxoI2lNpb0ckyFWT2JTj3MSC0dLYrKbWhVhCkfPZJo6MeaXXmPnmbKA8yjuLhHU2EbptTin4EFBNa+sO4zwKBgQCi1bILeYVwRiuBScR3hSHxODSjs54lmdn+pLCmNyFTEf1rW18Btb0odpMMqTxI8UNZDTeFf203zCwj5WQnl5R6x9lR6AbI6m98uF7ZO27cVygTJJ2E43TqcPJYv19kpPBtuLlspDtYVNvN1oUskpPTdfoYr0d7eStFlPQJUbB5AQKBgC8pfh6GaYRKB+GfLWe7EIZ86V8r/aTzGn/eFIJyl+b5ocXjb6LaysUz0A+UWe+jgiUYZOZ9fuY2rYhf9eVPQPjEWiwnwCM29HXCqvom0LcYus8vORsgHxgEHV5HM26YzoASECzsEoOAerfMHPlBrRQ2PrR+2bQev+VylhtO8rNI",
+		"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCm9OV6zH5DYH/ZnAVYHscEELdCNfNTHGuBv1nYYEY9FrOzE0/4kLl9f7Y9dkWHlc2ocDwbrFSm0Vqz0q2rJPxXUYBCQl5yW3jzuKSXif7q1yOwkFVtJXvuhf5WRy+1X5FOFoMvS7538No0RpnLzmNi3ktmiqmhpcY/1pmt20FHQQIDAQAB",
+		true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	// 扫码支付
 	// 请求参数
 	bm := make(gopay.BodyMap)
@@ -23,11 +31,11 @@ func TestClient_ScanPay(t *testing.T) {
 		Set("authcode", "131104796948096102")
 	// 创建订单
 	resp, err := client.ScanPay(ctx, bm)
-	xlog.Debugf("allRsp:%+v", resp)
 	if err != nil {
-		xlog.Errorf("%+v", err)
-		return
+		t.Fatal(err)
 	}
+	t.Logf("rsp:%+v", resp)
+
 }
 
 func TestClient_Query(t *testing.T) {
@@ -88,6 +96,14 @@ func TestClient_Close(t *testing.T) {
 }
 
 func TestClient_NativePay(t *testing.T) {
+	client, err = NewClient("660121065134QCZ", "00382908",
+		"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1izXRKdSXuwse3SDP9xDoHg3UnvNOq8RhcU/5479w5pYJiSTCkOrC45+2SsdItwJSxxZOnvRTZl46n/q/IoO5HDWSk45Ox2jY9Adii7GPyBJxl0Bl+skTKkMUovfbc1G4Yy+2mwhRd3JD7MWs42M/d91ENFYyQV9QF3f77TKriZX20o0apthnC88L2AFnBG8CkzK+B2izoMmbkffhC2L088UZQYUHHpC/GjLVcgdHwEzIlRSHwy1rn7PmYJ6s7hiwXd9Q/uqjncCJ9yunvwvxs6y00F2T9hvU7hvjn5xlW6DSY/MkXYLVblNWhYmD0ivCeKtx96SjdBocUXLG9WZHAgMBAAECggEAAfjIMtvAizBCKnsbSKzKHsQEzPusQfGUwKUZhAEhcHOTnIRbiyC6XX1ZnAIrjWbQ6rwopyPGHKyxma1om0HCtlGhZA63UYWMN58Ho1KN1BKB31JVyOiuxgzhGOnrf57fwaAH/BeTWHZywOhLz4ejo8czJVAiwbfjsDkCfMrp1iiQ/lLI+npHYnG9qGAuUqa9C0NSJlTP+tG1m4DBkodJIXPsmTmqTudav1cQKsMQr8zcf8yWtAdB9XhqTwqLAiLR76w2KWwRSxjtjCaX8QTITcjWX2Rpj4iCmuFT0qy0THZRwURX2faXO3GTg7e23wTJ6GdPmYqCbGME+ZSAvm81wQKBgQDa3Vo8qYYRcqraSJPbdFfQUR4DXwsdgdj/39dCSxIAWlYAKdsZiWjfwh3u69ENZxKYjEvJvil78KIahqggNAobARDV+2mSdsnTn0UxtTGxbF+AGXVARDK+GRXu/aN1qpuOBoXXRmTWfKLIsRnBjT2m4u14Rs+0shT17ByGcyB5FwKBgQDUWMgDe2ZV9oYxAo5PbrqXkLJ4Vw3nBPg4ETGHFvDPmrHTTL8ww0ZT4qLb4Bi0145GVvdf4fB4Q6rUfEBKTwmhHLW4HxjIGGmAPmiTfaMLhcS370uZ9q1YddYoMIkbpsfG1Ovlaw3BpsaN6HYIG7aD69s8Rzv1kiswqIPc0etaUQKBgQCxCj8VNPgbIwtreSwJaAokm4lQptAh9UgoatQAbyNHn8tTZIg0Fv/7iFWYT68STV8wgMRJlAaQmC04kdZr/kxyXaFVxoI2lNpb0ckyFWT2JTj3MSC0dLYrKbWhVhCkfPZJo6MeaXXmPnmbKA8yjuLhHU2EbptTin4EFBNa+sO4zwKBgQCi1bILeYVwRiuBScR3hSHxODSjs54lmdn+pLCmNyFTEf1rW18Btb0odpMMqTxI8UNZDTeFf203zCwj5WQnl5R6x9lR6AbI6m98uF7ZO27cVygTJJ2E43TqcPJYv19kpPBtuLlspDtYVNvN1oUskpPTdfoYr0d7eStFlPQJUbB5AQKBgC8pfh6GaYRKB+GfLWe7EIZ86V8r/aTzGn/eFIJyl+b5ocXjb6LaysUz0A+UWe+jgiUYZOZ9fuY2rYhf9eVPQPjEWiwnwCM29HXCqvom0LcYus8vORsgHxgEHV5HM26YzoASECzsEoOAerfMHPlBrRQ2PrR+2bQev+VylhtO8rNI",
+		"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCm9OV6zH5DYH/ZnAVYHscEELdCNfNTHGuBv1nYYEY9FrOzE0/4kLl9f7Y9dkWHlc2ocDwbrFSm0Vqz0q2rJPxXUYBCQl5yW3jzuKSXif7q1yOwkFVtJXvuhf5WRy+1X5FOFoMvS7538No0RpnLzmNi3ktmiqmhpcY/1pmt20FHQQIDAQAB",
+		true)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	expire := time.Now().Add(10 * time.Minute).Format("20060102150405")
 	bm := make(gopay.BodyMap)
 	bm.Set("reqsn", "nativelarry01").Set("trxamt", "1").Set("body", "支付测试").Set("expiretime", expire)
