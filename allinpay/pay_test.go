bf1f111edd55e0f240f6d50454e72ab0719af9c5
Shutian.Ka <shutian_ka@163.com>
2026-06-26 11:40:34 +0800

    refactor(allinpay): 更新测试代码中的客户端初始化和日志记录
    - 移除硬编码的客户端配置参数
    - 将日志记录从 t.Logf 和 t.Fatal 替换为 xlog 包
    - 增加本地支付测试中的过期时间从 10 分钟到 120 分钟
    - 在所有测试函数中统一使用 xlog 进行调试和错误日志记录
    - 移除测试输出注释中的敏感信息并更新响应结构体字段说明

--- allinpay/pay_test.go
+++ allinpay/pay_test.go
@@ -10,14 +10,6 @@ import (
 )
 
 func TestClient_ScanPay(t *testing.T) {
-	client, err = NewClient("660121065134QCZ", "00382908",
-		"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1izXRKdSXuwse3SDP9xDoHg3UnvNOq8RhcU/5479w5pYJiSTCkOrC45+2SsdItwJSxxZOnvRTZl46n/q/IoO5HDWSk45Ox2jY9Adii7GPyBJxl0Bl+skTKkMUovfbc1G4Yy+2mwhRd3JD7MWs42M/d91ENFYyQV9QF3f77TKriZX20o0apthnC88L2AFnBG8CkzK+B2izoMmbkffhC2L088UZQYUHHpC/GjLVcgdHwEzIlRSHwy1rn7PmYJ6s7hiwXd9Q/uqjncCJ9yunvwvxs6y00F2T9hvU7hvjn5xlW6DSY/MkXYLVblNWhYmD0ivCeKtx96SjdBocUXLG9WZHAgMBAAECggEAAfjIMtvAizBCKnsbSKzKHsQEzPusQfGUwKUZhAEhcHOTnIRbiyC6XX1ZnAIrjWbQ6rwopyPGHKyxma1om0HCtlGhZA63UYWMN58Ho1KN1BKB31JVyOiuxgzhGOnrf57fwaAH/BeTWHZywOhLz4ejo8czJVAiwbfjsDkCfMrp1iiQ/lLI+npHYnG9qGAuUqa9C0NSJlTP+tG1m4DBkodJIXPsmTmqTudav1cQKsMQr8zcf8yWtAdB9XhqTwqLAiLR76w2KWwRSxjtjCaX8QTITcjWX2Rpj4iCmuFT0qy0THZRwURX2faXO3GTg7e23wTJ6GdPmYqCbGME+ZSAvm81wQKBgQDa3Vo8qYYRcqraSJPbdFfQUR4DXwsdgdj/39dCSxIAWlYAKdsZiWjfwh3u69ENZxKYjEvJvil78KIahqggNAobARDV+2mSdsnTn0UxtTGxbF+AGXVARDK+GRXu/aN1qpuOBoXXRmTWfKLIsRnBjT2m4u14Rs+0shT17ByGcyB5FwKBgQDUWMgDe2ZV9oYxAo5PbrqXkLJ4Vw3nBPg4ETGHFvDPmrHTTL8ww0ZT4qLb4Bi0145GVvdf4fB4Q6rUfEBKTwmhHLW4HxjIGGmAPmiTfaMLhcS370uZ9q1YddYoMIkbpsfG1Ovlaw3BpsaN6HYIG7aD69s8Rzv1kiswqIPc0etaUQKBgQCxCj8VNPgbIwtreSwJaAokm4lQptAh9UgoatQAbyNHn8tTZIg0Fv/7iFWYT68STV8wgMRJlAaQmC04kdZr/kxyXaFVxoI2lNpb0ckyFWT2JTj3MSC0dLYrKbWhVhCkfPZJo6MeaXXmPnmbKA8yjuLhHU2EbptTin4EFBNa+sO4zwKBgQCi1bILeYVwRiuBScR3hSHxODSjs54lmdn+pLCmNyFTEf1rW18Btb0odpMMqTxI8UNZDTeFf203zCwj5WQnl5R6x9lR6AbI6m98uF7ZO27cVygTJJ2E43TqcPJYv19kpPBtuLlspDtYVNvN1oUskpPTdfoYr0d7eStFlPQJUbB5AQKBgC8pfh6GaYRKB+GfLWe7EIZ86V8r/aTzGn/eFIJyl+b5ocXjb6LaysUz0A+UWe+jgiUYZOZ9fuY2rYhf9eVPQPjEWiwnwCM29HXCqvom0LcYus8vORsgHxgEHV5HM26YzoASECzsEoOAerfMHPlBrRQ2PrR+2bQev+VylhtO8rNI",
-		"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCm9OV6zH5DYH/ZnAVYHscEELdCNfNTHGuBv1nYYEY9FrOzE0/4kLl9f7Y9dkWHlc2ocDwbrFSm0Vqz0q2rJPxXUYBCQl5yW3jzuKSXif7q1yOwkFVtJXvuhf5WRy+1X5FOFoMvS7538No0RpnLzmNi3ktmiqmhpcY/1pmt20FHQQIDAQAB",
-		true)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	// 扫码支付
 	// 请求参数
 	bm := make(gopay.BodyMap)
@@ -31,11 +23,11 @@ func TestClient_ScanPay(t *testing.T) {
 		Set("authcode", "131104796948096102")
 	// 创建订单
 	resp, err := client.ScanPay(ctx, bm)
+	xlog.Debugf("aliRsp:%+v", resp)
 	if err != nil {
-		t.Fatal(err)
+		xlog.Errorf("%+v", err)
+		return
 	}
-	t.Logf("rsp:%+v", resp)
-
 }
 
 func TestClient_Query(t *testing.T) {
@@ -96,22 +88,15 @@ func TestClient_Close(t *testing.T) {
 }
 
 func TestClient_NativePay(t *testing.T) {
-	client, err = NewClient("660121065134QCZ", "00382908",
-		"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC1izXRKdSXuwse3SDP9xDoHg3UnvNOq8RhcU/5479w5pYJiSTCkOrC45+2SsdItwJSxxZOnvRTZl46n/q/IoO5HDWSk45Ox2jY9Adii7GPyBJxl0Bl+skTKkMUovfbc1G4Yy+2mwhRd3JD7MWs42M/d91ENFYyQV9QF3f77TKriZX20o0apthnC88L2AFnBG8CkzK+B2izoMmbkffhC2L088UZQYUHHpC/GjLVcgdHwEzIlRSHwy1rn7PmYJ6s7hiwXd9Q/uqjncCJ9yunvwvxs6y00F2T9hvU7hvjn5xlW6DSY/MkXYLVblNWhYmD0ivCeKtx96SjdBocUXLG9WZHAgMBAAECggEAAfjIMtvAizBCKnsbSKzKHsQEzPusQfGUwKUZhAEhcHOTnIRbiyC6XX1ZnAIrjWbQ6rwopyPGHKyxma1om0HCtlGhZA63UYWMN58Ho1KN1BKB31JVyOiuxgzhGOnrf57fwaAH/BeTWHZywOhLz4ejo8czJVAiwbfjsDkCfMrp1iiQ/lLI+npHYnG9qGAuUqa9C0NSJlTP+tG1m4DBkodJIXPsmTmqTudav1cQKsMQr8zcf8yWtAdB9XhqTwqLAiLR76w2KWwRSxjtjCaX8QTITcjWX2Rpj4iCmuFT0qy0THZRwURX2faXO3GTg7e23wTJ6GdPmYqCbGME+ZSAvm81wQKBgQDa3Vo8qYYRcqraSJPbdFfQUR4DXwsdgdj/39dCSxIAWlYAKdsZiWjfwh3u69ENZxKYjEvJvil78KIahqggNAobARDV+2mSdsnTn0UxtTGxbF+AGXVARDK+GRXu/aN1qpuOBoXXRmTWfKLIsRnBjT2m4u14Rs+0shT17ByGcyB5FwKBgQDUWMgDe2ZV9oYxAo5PbrqXkLJ4Vw3nBPg4ETGHFvDPmrHTTL8ww0ZT4qLb4Bi0145GVvdf4fB4Q6rUfEBKTwmhHLW4HxjIGGmAPmiTfaMLhcS370uZ9q1YddYoMIkbpsfG1Ovlaw3BpsaN6HYIG7aD69s8Rzv1kiswqIPc0etaUQKBgQCxCj8VNPgbIwtreSwJaAokm4lQptAh9UgoatQAbyNHn8tTZIg0Fv/7iFWYT68STV8wgMRJlAaQmC04kdZr/kxyXaFVxoI2lNpb0ckyFWT2JTj3MSC0dLYrKbWhVhCkfPZJo6MeaXXmPnmbKA8yjuLhHU2EbptTin4EFBNa+sO4zwKBgQCi1bILeYVwRiuBScR3hSHxODSjs54lmdn+pLCmNyFTEf1rW18Btb0odpMMqTxI8UNZDTeFf203zCwj5WQnl5R6x9lR6AbI6m98uF7ZO27cVygTJJ2E43TqcPJYv19kpPBtuLlspDtYVNvN1oUskpPTdfoYr0d7eStFlPQJUbB5AQKBgC8pfh6GaYRKB+GfLWe7EIZ86V8r/aTzGn/eFIJyl+b5ocXjb6LaysUz0A+UWe+jgiUYZOZ9fuY2rYhf9eVPQPjEWiwnwCM29HXCqvom0LcYus8vORsgHxgEHV5HM26YzoASECzsEoOAerfMHPlBrRQ2PrR+2bQev+VylhtO8rNI",
-		"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCm9OV6zH5DYH/ZnAVYHscEELdCNfNTHGuBv1nYYEY9FrOzE0/4kLl9f7Y9dkWHlc2ocDwbrFSm0Vqz0q2rJPxXUYBCQl5yW3jzuKSXif7q1yOwkFVtJXvuhf5WRy+1X5FOFoMvS7538No0RpnLzmNi3ktmiqmhpcY/1pmt20FHQQIDAQAB",
-		true)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expire := time.Now().Add(10 * time.Minute).Format("20060102150405")
+	expire := time.Now().Add(120 * time.Minute).Format("20060102150405")
 	bm := make(gopay.BodyMap)
 	bm.Set("reqsn", "nativelarry01").Set("trxamt", "1").Set("body", "支付测试").Set("expiretime", expire)
 	resp, err := client.NativePay(ctx, bm)
+	xlog.Debugf("aliRsp:%+v", resp)
 	if err != nil {
-		t.Fatal(err)
+		xlog.Errorf("%+v", err)
+		return
 	}
-	t.Logf("rsp:%+v", resp)
 
 	// Output:
 	// &{RspBase:{RetCode:SUCCESS RetMsg: Sign:*** Cusid:*** Appid:***} ReqSn:nativelarry01 TrxStatus:0000 ErrMsg:生成收款码成功 PayInfo:https://syb.allinpay.com/apiweb/h5unionpay/unionnative?token=** RandomStr:636549522285}
@@ -119,10 +104,11 @@ func TestClient_NativePay(t *testing.T) {
 
 func TestClient_NativeClose(t *testing.T) {
 	resp, err := client.NativeClose(ctx, OrderTypeReqSN, "nativelarry01")
+	xlog.Debugf("aliRsp:%+v", resp)
 	if err != nil {
-		t.Fatal(err)
+		xlog.Errorf("%+v", err)
+		return
 	}
-	t.Logf("rsp:%+v", resp)
 
 	// Output:
 	// &{RspBase:{RetCode:SUCCESS RetMsg: Sign:**** Cusid:***** Appid:*****} TrxStatus:0000 ErrMsg:交易关闭成功 RandomStr:476373212870}
@@ -130,8 +116,12 @@ func TestClient_NativeClose(t *testing.T) {
 
 func TestClient_QueryConfirm(t *testing.T) {
 	resp, err := client.QueryConfirm(ctx, OrderTypeReqSN, "nativelarry01")
+	xlog.Debugf("aliRsp:%+v", resp)
 	if err != nil {
-		t.Fatal(err)
+		xlog.Errorf("%+v", err)
+		return
 	}
-	t.Logf("rsp:%+v", resp)
+
+	// Output:
+	// &{RspBase:{RetCode:SUCCESS RetMsg: Sign:*** Cusid:** Appid:***} Trxid:260626128079233486 ChnlTrxId: Reqsn:nativelarry01 TrxStatus:3045 Acct:opn0buM4VhGG17vZWkMLip9v1CNU TrxCode:VSP501 FinTime:20260626105619 ErrMsg:订单置关闭 RandomStr:045082197576 TrxAmt:1 Cmid:990361082416001 Chnlid: InitAmt:1 Fee: ChnlData: AcctType:99}
 }
